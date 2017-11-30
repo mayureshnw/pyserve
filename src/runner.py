5b1b1c08e10d1872534d679079fd70735c468133
Mayuresh <mayuresh2212@gmail.com>
2017-11-29 20:12:58 -0500

    basic structure for server

--- src/runner.py
+++ src/runner.py
@@ -1,47 +1,51 @@
 import socket
-#
-# HOST, PORT = '', 8888
-#
-#
-# print('Serving HTTP on port 8888 ...')
-# while True:
-#     client_connection, client_address = listen_socket.accept()
-#     request = client_connection.recv(1024)
-#     print(request)
-#
-#     http_response = """\
-# HTTP/1.1 200 OK
-#
-# Hello, World!
-# """
-#     client_connection.sendall(bytes(http_response, 'UTF-8'))
-#     client_connection.close()
+
+addressFamily = socket.AF_INET
+socketType = socket.SOCK_STREAM
+queueSize = 1
+HOST = ''
+PORT = 8080
+CLIENT_DATA_BUFFER_SIZE = 1024
 
 
-class Server(object):
-    """docstring for Server."""
+def init(host, port):
+    socketListener = socket.socket(
+        addressFamily,
+        socketType
+    )
 
-    def __init__(self, host, port):
-        super(Server, self).__init__()
-        self.HOST = host
-        self.PORT = port
+    socketListener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    socketListener.bind((host, port))
+    socketListener.listen(queueSize)
 
-        print(self.HOST, self.PORT)
+    return socketListener
 
-        self.listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.listen_socket.setsockopt(
-            socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self.listen_socket.bind((self.HOST, self.PORT))
-        self.listen_socket.listen(1)
 
-    def run(self):
-        while True:
-            client_connection, client_address = self.listen_socket.accept()
-            request = client_connection.recv(1024)
-            print(request)
+def requestParse(req):
+    print(req)
 
-            http_response = """\HTTP/1.1 200 OK
-            Hello, World!
-            """
-            client_connection.sendall(bytes(http_response, 'UTF-8'))
-            client_connection.close()
+
+def generateResponse(parsedReq):
+    return """\
+HTTP/1.1 200 OK
+
+Hello, World!
+"""
+
+
+def handleSingleRequest(connection):
+    req = connection.recv(CLIENT_DATA_BUFFER_SIZE)
+    requestParse(req)
+    res = generateResponse(req)
+    connection.sendall(bytes(res, 'UTF-8'))
+    connection.close()
+
+
+def run():
+    socketListener = init(HOST, PORT)
+    while True:
+        connection, address = socketListener.accept()
+        handleSingleRequest(connection)
+
+
+run()
