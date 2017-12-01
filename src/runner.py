18ef79c01bc77b774892aa7be7fa0ccfb43ae360
Mayuresh <mayuresh2212@gmail.com>
2017-12-01 02:56:11 -0500

    Closes #3 , Closes #4
    1. Generate response from the callable
    2. use django response object to get status code and serialized headers
    3. shameful hack to remove content length
    4. One request at a time. ! yolo !

--- src/runner.py
+++ src/runner.py
@@ -1,6 +1,7 @@
 import socket
 import sys
 from io import StringIO
+from .statuscodes import getStatusDefinition
 
 addressFamily = socket.AF_INET
 socketType = socket.SOCK_STREAM
@@ -24,24 +25,75 @@ def init(host, port):
 
 
 def requestParse(req):
+    print(">> In requestParse")
     req = req.decode('UTF-8').splitlines()[0]
     req = req.rstrip('\r\n')
     reqlist = req.split()
     # Break down the request line into components
+    print(">> before return, exiting requestParse")
     return {'request_method': reqlist[0],
             'path': reqlist[1],
             'request_version': reqlist[2]}
 
 
-def generateResponse(parsedReq):
-    return """\
-HTTP/1.1 200 OK
+def generateResponse(status, headersByteString, res):
+    status_def = 'OK'
+    try:
+        status_def = getStatusDefinition(status)
+    except:
+        status = 500
+        status_def = getStatusDefinition(status)
+    response = 'HTTP/1.1 {status} {status_def}\r\n'.format(
+        status=status, status_def=status_def)
 
-Hello, World!
-"""
+    response = bytes(response, 'UTF-8')
+    response += headersByteString
+    response += bytes('\r\n', 'UTF-8')
+
+    for data in res:
+        response += data
+
+    return response
 
 
-def handleSingleRequest(connection):
+def start_response(status, response_headers, exec_info=None):
+    """
+    PEP333 start_response method signature
+    start_response(status, response_headers, exc_info=None)
+
+    Supplied as second parameter to application callable
+
+    NOTE: Interstingly it seems like write() callableshould be able to writ to the output stream straight away.
+    Since we are not implementing outut asstreams per se, we'll sip this for now.
+
+    A parallell argument seems to be that no one realy uses it either (doubtful about that).
+    Adding a todo for same
+
+    TODO: return write() cllable to end dat to output stream
+    SHOULD Return write(body_data) callable
+    """
+
+    preset_headers = [('Server', 'WSGIServer 1.0')]
+
+    # Following HTTP Spec https://www.w3.org/Protocols/rfc2616/rfc2616-sec6.html
+
+    # status line format ==> Status-Line = HTTP-Version SP Status-Code SP Reason-Phrase CRLF
+    # if status == None:
+    #     status = 500
+    # status_def = getStatusDefinition(status)
+
+    # Application sending error informaton as a part of staus
+    res_status_line = "HTTP/1.1 {status} \r\n".format(status=status)
+    res_headers = preset_headers + response_headers
+    response = res_status_line
+
+    for header in res_headers:
+        response += '{0}: {1}\r\n'.format(*header)
+        response += '\r\n'
+
+
+def handleSingleRequest(connection, app):
+    print(">> in handleSingleRequest")
     req = connection.recv(CLIENT_DATA_BUFFER_SIZE)
     reqDetails = requestParse(req)
 
@@ -49,20 +101,40 @@ def handleSingleRequest(connection):
     reqDetails['request_data'] = req.decode('UTF-8')
 
     # reqDetails contains data for cgi variables
-    set_wsgi(reqDetails)
-    res = generateResponse(req)
-    connection.sendall(bytes(res, 'UTF-8'))
+    env = set_wsgi(reqDetails)
+
+    print(">> calling app callable")
+    response = app(env, start_response)
+    print()
+    print(response.status_code)
+    print(">> app call success, response recieved")
+    print(">> generating response")
+
+    headers = response._headers
+    del headers['content-length']
+    response._headers = headers
+    #
+    # res = bytes('', 'UTF-8')
+
+    response = generateResponse(
+        response.status_code, response.serialize_headers(), response)
+
+    print(response)
+    connection.sendall(response)
     connection.close()
+    print(">> connection closed, exiting handleSingleRequest")
 
 
-def run():
+def run(application):
     socketListener = init(HOST, PORT)
     while True:
+        print(">> in while True")
         connection, address = socketListener.accept()
-        handleSingleRequest(connection)
+        handleSingleRequest(connection, application)
 
 
 def set_wsgi(cgiVariables):
+    print(">> in set_wsgi")
     wsgiconfig = {}
     wsgiconfig['wsgi.version'] = (1, 0)  # works but why ?
     wsgiconfig['wsgi.url_scheme'] = 'http'
@@ -75,9 +147,11 @@ def set_wsgi(cgiVariables):
     # CGI VARIABLES
     wsgiconfig['REQUEST_METHOD'] = cgiVariables['request_method']  # GET
     wsgiconfig['PATH_INFO'] = cgiVariables['path']  # /hello
-    wsgiconfig['SERVER_NAME'] = socket.getfqdn(HOST)  # localhost
+    # wsgiconfig['SERVER_NAME'] = socket.getfqdn(HOST)  # localhost
+    wsgiconfig['SERVER_NAME'] = '127.0.0.1'
     wsgiconfig['SERVER_PORT'] = PORT
 
+    print(">> exiting set_wsgi")
     return wsgiconfig
 
 
@@ -94,4 +168,4 @@ def main(sysargs):
     app = getattr(imported_application, app)
     print(app)
 
-    run()
+    run(app)
